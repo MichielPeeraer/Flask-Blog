4986882b79664f922ec9d640aedd6829e76a893d
Michiel Peeraer <michiel.peeraer@student.kdg.be>
2020-05-19 23:16:59 +0200

    Added login form

--- flaskblog.py
+++ flaskblog.py
@@ -35,9 +35,17 @@ def register():
 
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == 'test@test.test' and form.password.data == 'test':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login unsuccessful. Please check your username and/or password', 'danger')
+
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/about")
