d2df7467593ee7e2d92ecd71c704ef85737b788c
Michiel Peeraer <michiel.peeraer@student.kdg.be>
2020-05-19 16:38:15 +0200

    using template inheritance

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'J.K. Rowling',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date': 'May 18, 2020'
+    },
+    {
+        'author': 'Stephen King',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date': 'May 19, 2020'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
