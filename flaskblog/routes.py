88672521449e45d621b8b1d4609210842c7e0aa4
Michiel Peeraer <michiel.peeraer@student.kdg.be>
2020-05-20 20:14:18 +0200

    add account page
    -add login required pages
    -add request next page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -19,11 +19,6 @@ posts = [
     }
 ]
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', posts=posts)
-
 @app.route("/register", methods=['GET', 'POST'])
 def register():
 
@@ -36,11 +31,13 @@ def register():
 
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+
         db.session.add(user)
         db.session.commit()
 
         login_user(user)
         flash(f'Your account has been successfully created!', 'success')
+
         return redirect(url_for('home'))
 
     return render_template('register.html', title='Register', form=form)
@@ -57,9 +54,16 @@ def login():
 
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
+
             login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
             flash(f'You are logged in!', 'success')
-            return redirect(url_for('home'))
+
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+
         else:
             flash(f'Login unsuccessful. Please check your email and/or password', 'danger')
 
@@ -69,6 +73,16 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
 
 @app.route("/about")
 def about():
