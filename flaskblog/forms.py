c371fc9a29eb637e43e00cadf8ea5a511cea10fb
Michiel Peeraer <michiel.peeraer@student.kdg.be>
2020-05-21 12:55:13 +0200

    Update forms.py
    -deleted spaces

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -8,13 +8,9 @@ from flaskblog.models import User
 class RegistrationForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-
     email = StringField('Email', validators=[DataRequired(), Email()])
-
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
-
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -32,19 +28,14 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
 
     email = StringField('Email', validators=[DataRequired(), Email()])
-
     password = PasswordField('Password', validators=[DataRequired()])
-
     remember = BooleanField('Remember Me')
-
     submit = SubmitField('Login')
 
 class UpdateAccountForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-
     email = StringField('Email', validators=[DataRequired(), Email()])
-
     submit = SubmitField('Save Changes')
 
     def validate_username(self, username):
